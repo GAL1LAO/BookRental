0bf16bc78a90b2eb7baea9b8ace89e7e52b7e4e8
Martin Schollmeier <90894119+schollmeier@users.noreply.github.com>
2023-09-07 12:11:26 +0200

    added user specifications to profile

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,11 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function ProfileScreen() {
+  const adress = 'Prof. Dr. Med'
+  const userName = 'Max Mustermann'
+  const role = 'Schulleiter'
+  const mail = 'max.mustermann@bestegrundschule.de'
+  const phoneNumber = '0176 12345678'
+  const birthDate = '01.11.1478'
+
   return (
     <View style={styles.container}>
-      <Text>Profile</Text>
-      <StatusBar style="auto" />
+      <View style={styles.captionContainer}>
+        <Text style={styles.captionText}>PROFIL</Text>
+        <Text style={styles.subCaptionText}>{adress} {userName}</Text>
+      </View>
+      <View style={styles.userDetails}>
+        <View style={styles.column1}>
+          <Text style={styles.text}>Rolle:</Text>  
+        </View>
+        <View style={styles.column2}>
+          <Text style={styles.text}>{role}</Text>
+        </View>
+      </View>
+      <View style={styles.userDetails}>
+        <View style={styles.column1}>
+          <Text style={styles.text}>E-Mail:</Text>
+        </View>
+        <View style={styles.column2}>
+          <Text style={styles.text}>{mail}</Text>
+        </View>
+      </View><View style={styles.userDetails}>
+        <View style={styles.column1}>
+          <Text style={styles.text}>Telefonnummer:</Text>
+        </View>
+        <View style={styles.column2}>
+          <Text style={styles.text}>{phoneNumber}</Text>
+        </View>
+      </View>
+      <View style={styles.userDetails}>
+        <View style={styles.column1}>
+          <Text style={styles.text}>Geburtsdatum:</Text>
+        </View>
+        <View style={styles.column2}>
+          <Text style={styles.text}>{birthDate}</Text>
+        </View>
+      </View>
+      <View
+        style={styles.line}
+      />
     </View>
   );
 }
@@ -14,7 +56,40 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  captionContainer: {
+    paddingLeft: 10,
+    paddingTop: 10,
+  },
+  userDetails: {
+    flexDirection: 'row',
+    paddingLeft: 10,
+    paddingTop: 10,
+    alignItems: 'flex-start'
+  },
+  column1: {
+    width: '40%',
+    paddingLeft: 10,
+    paddingTop: 10,
+  },
+  column2: {
+    width: '60%',
+    paddingLeft: 10,
+    paddingTop: 10,
+  },
+  captionText: {
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  subCaptionText: {
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  line: {
+    marginHorizontal: 5,
+    marginVertical: 10,
+    borderBottomColor: 'black',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  }
+
 });
