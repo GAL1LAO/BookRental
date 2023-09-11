bc8ef8cba90e0812b64dd01985e3b22460234f53
GAL1LAO <adrian.nikaeen81@gmail.com>
2023-09-11 12:28:55 +0200

    changes to routes.js for items

--- routes.js
+++ routes.js
@@ -42,6 +42,33 @@ app.post('/login', (req, res) => {
 });
 
 
+app.get('/items', async (req, res) => {
+  // Connecting to the database.
+  // Connecting to the database.
+  connection.getConnection(function (err, connection) {
+    // Executing the MySQL query (select all data from the 'users' table).
+    connection.query('SELECT * FROM Items', function (error, results, fields) {
+      // If some error occurs, we throw an error.
+      if (error) throw error;
+
+      // Getting the 'response' from the database and sending it to our route. This is were the data is.
+      res.send(results)
+    });
+  });
+});
+  
+//   try {
+//     const items = await db.Item.findAll({
+//       attributes: ['type', 'user_short'],
+//     });
+//     res.json(items);
+//   } catch (error) {
+//     console.error('Error fetching data:', error);
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// });
+
+
 // Starting our server.
 app.listen(3000, () => {
  console.log('Go to http://localhost:3000/users so you can see the data.');
