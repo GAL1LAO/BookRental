3332529ceaca3f4bf287bc58a19ea58cc4f58c13
GAL1LAO <adrian.nikaeen81@gmail.com>
2023-09-20 17:31:43 +0200

    admin buttons gefixed

--- navigation/screens/Admin.js
+++ navigation/screens/Admin.js
@@ -94,11 +94,7 @@ const serverUrl = 'http://'+ process.env.localIP +':3000'
         }
         return(
             <View style={styles.container}>
-                <TouchableOpacity style={styles.fakeButton} onPress={() => {dispatch({type : 'Home'})}}>
-                    <Text style={styles.subCaptionTextWhite}>
-                        Zurück zur Übersicht
-                    </Text>
-                </TouchableOpacity>
+               
                 <Text>Add item</Text>
                 <View style={styles.inputContainer}>
                     <Ionicons style={styles.inputIcon} name="person" size={20} color="#000"/>
@@ -163,11 +159,18 @@ const serverUrl = 'http://'+ process.env.localIP +':3000'
                         onChangeText={storageSite =>setStorageSite(storageSite)}
                     />
                 </View>
-                <TouchableOpacity style={styles.fakeButton} type='submit' onPress={async() => await addItem()}>
+                <View style={styles.buttonAlignment}>
+                <TouchableOpacity style={styles.column1} type='submit' onPress={async() => await addItem()}>
                     <Text style={styles.subCaptionTextWhite}>
-                        Item hinzufügen
+                        Hinzufügen
                     </Text>
                 </TouchableOpacity>
+                <TouchableOpacity style={styles.column2} onPress={() => {dispatch({type : 'Home'})}}>
+                    <Text style={styles.subCaptionTextWhite}>
+                        Abbrechen
+                    </Text>
+                </TouchableOpacity>
+                </View>
             </View>
         )
     }
@@ -223,11 +226,7 @@ const serverUrl = 'http://'+ process.env.localIP +':3000'
         }
         return(
             <View style={styles.container}>
-                <TouchableOpacity style={styles.fakeButton} onPress={() => {dispatch({type : 'Home'})}}>
-                    <Text style={styles.subCaptionTextWhite}>
-                        Zurück zur Übersicht
-                    </Text>
-                </TouchableOpacity>
+               
                 <Text>Add user</Text>
                 <View style={styles.inputContainer}>
                     <Ionicons style={styles.inputIcon} name="person" size={20} color="#000"/>
@@ -323,11 +322,18 @@ const serverUrl = 'http://'+ process.env.localIP +':3000'
                         onChangeText={password =>setPassword(password)}
                     />
                 </View>
-                <TouchableOpacity style={styles.fakeButton} type='submit' onPress={async() => await addUser()}>
+                <View style={styles.buttonAlignment}>
+                <TouchableOpacity style={styles.column1} type='submit' onPress={async() => await addItem()}>
                     <Text style={styles.subCaptionTextWhite}>
-                        Benutzer hinzufügen
+                        Hinzufügen
                     </Text>
                 </TouchableOpacity>
+                <TouchableOpacity style={styles.column2} onPress={() => {dispatch({type : 'Home'})}}>
+                    <Text style={styles.subCaptionTextWhite}>
+                        Abbrechen
+                    </Text>
+                </TouchableOpacity>
+                </View>
             </View>
         )
     }
@@ -383,6 +389,16 @@ const styles = StyleSheet.create({
         backgroundColor: '#3EB489',
         padding: 10
     },
+    fakeButtonCancel: {
+        marginTop: 20,
+        borderRadius: 10,
+        paddingVertical: 5,
+        marginVertical: 5,
+        alignItems: 'center',
+        width: '100%',
+        backgroundColor: 'red',
+        padding: 10
+    },
     subCaptionTextWhite: {
         fontWeight: 'bold',
         fontSize: 30,
@@ -408,4 +424,35 @@ const styles = StyleSheet.create({
         paddingLeft: 0,
         color: '#424242',
     },
+    buttonAlignment: { 
+    flexDirection: 'row',
+    paddingLeft: 10,
+    paddingTop: 10,
+    alignItems: 'flex-start',
+    width: '100%',
+  },
+  column1: {
+    width: '50%',
+    padding: 10,
+    marginTop: 20,
+    marginRight: 10,
+        borderRadius: 10,
+        paddingVertical: 5,
+        marginVertical: 5,
+        alignItems: 'center',
+        backgroundColor: '#3EB489',
+        padding: 10
+  },
+  column2: {
+    width: '50%',
+    padding: 10, 
+    marginTop: 20,
+    marginLeft: 10,
+        borderRadius: 10,
+        paddingVertical: 5,
+        marginVertical: 5,
+        alignItems: 'center',
+        backgroundColor: 'red',
+        padding: 10
+  },
 });
