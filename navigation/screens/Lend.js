fdcc3dcbebfa8e8e8b5d52cd68685fbf493a3120
jonathan-robottos <jonas-sobotta@web.de>
2023-09-21 12:03:17 +0200

    configured native wind

--- navigation/screens/Lend.js
+++ navigation/screens/Lend.js
@@ -1,18 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
   export default function LendScreen({navigation}){
   return (
-    <View style={styles.container}>
+    <View className="flex-1 items-center justify-center bg-white">
       <Text>Lend</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
