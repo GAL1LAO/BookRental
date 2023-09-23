050031bfc0c255697ca61f811af5734c1552e60e
jonathan-robottos <jonas-sobotta@web.de>
2023-09-23 11:58:37 +0200

    add /lendItem endpoint

--- navigation/screens/QRScanner.js
+++ navigation/screens/QRScanner.js
@@ -7,6 +7,8 @@ export default function QRScannerScreen() {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
 
+  const serverUrl = 'http://'+ process.env.localIP +':3000'
+
   useEffect(() => {
     (async () => {
       const { status } = await BarCodeScanner.requestPermissionsAsync();
@@ -17,6 +19,12 @@ export default function QRScannerScreen() {
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
     alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+
+    //make API call to DB
+    
+    //if successful, navigate to success screen
+    //else navigate to error screen
+
   };
 
   if (hasPermission === null) {
--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
-const cors = require('cors')
+const cors = require('cors');
+const dayjs = require('dayjs');
 require('dotenv').config({path: './.env'});
 const connection = mysql.createPool({
   host     : 'localhost',
@@ -41,20 +42,232 @@ app.post('/login', (req, res) => {
 });
 });
 
+app.post('/userByShort', (req, res) => {
+  connection.getConnection(function (err, connection) {
+  const loginQuery = 'SELECT * FROM Users WHERE short = "' + req.body.short + '"' 
+  connection.query(loginQuery, function (error, results, fields) {
+    if (error) throw error;
+    console.log(results)
+    res.send(results)
+  });
+});
+})
 
-app.get('/items', async (req, res) => {
+app.post('/editUser', (req, res) => {
+  connection.getConnection(function (err, connection) {
+  const loginQuery = 'UPDATE Users SET '+
+  'title = "' + req.body.title + '",'+
+  'firstName = "' + req.body.firstName + '",'+
+  'lastName = "' + req.body.lastName + '",'+
+  'mailAddress = "' + req.body.mailAddress + '",'+
+  'phoneNumber = "' + req.body.phoneNumber + '",'+
+  'birthDate = "' + req.body.birthDate + '",'+
+  'role = "' + req.body.role + '"'+
+  'WHERE short = "' + req.body.short + '"' 
+  connection.query(loginQuery, function (error, results, fields) {
+    if (error) throw error;
+    console.log(results)
+    res.send(results)
+  });
+});
+})
+
+app.post('/editItem', (req, res) => {
+  connection.getConnection(function (err, connection) {
+  const updateItemQuery = 'UPDATE Items SET '+
+  'type = "' + req.body.type + '",'+
+  'name = "' + req.body.name + '",'+
+  'description = "' + req.body.description + '",'+
+  'storageSite = "' + req.body.storageSite + '",'+
+  'dateOfPurchase = "' + req.body.dateOfPurchase + '" ' +
+  'WHERE ID = "' + req.body.id + '"' 
+  console.log(updateItemQuery)
+  connection.query(updateItemQuery, function (error, results, fields) {
+    if (error) throw error;
+    console.log(results)
+    res.send(results)
+  });
+});
+})
+
+app.put('/lendItem', (req, res) => {
+  connection.getConnection(function (err, connection) {
+  const lendItemQuery = 'UPDATE Items SET '+
+  'user_short = "' + req.body.short + '"'+
+  'WHERE ID = "' + req.body.id + '"'
+  console.log(lendItemQuery)
+  connection.query(lendItemQuery, function (error, results, fields) {
+    if (error) throw error;
+    console.log(results)
+    res.send(results)
+  }
+  );
+});
+})
+
+app.post('/deleteItem', (req, res) => {
+  connection.getConnection(function (err, connection) {
+  const queries = [
+  'DELETE FROM Audit WHERE item_ID = "' + req.body.id+ '"',
+  'DELETE FROM Damages WHERE item_ID = "' + req.body.id+ '"',
+  'DELETE FROM Reservations WHERE item_ID = "' + req.body.id+ '"',
+  'DELETE FROM Items WHERE ID = "' + req.body.id+ '"'
+  ]
+  console.log(queries)
+  for(const query of queries){
+    connection.query(query, function (error, results, fields) {
+      if (error) throw error;
+      console.log(results)
+    });
+  }
+});
+})
+
+app.post('/addUser', async (req, res) => {
+  connection.getConnection(function (err, connection) {
+    const existingUserQuery = 'SELECT * FROM Users WHERE short = "' + req.body.short + '"'
+    connection.query(existingUserQuery, function (error, existingUserResults, fields) {
+      if (error) throw error;
+      console.log(existingUserResults)
+      if(existingUserResults.length>0){
+        res.send({existingUser : true})
+        return
+      }else{
+        const addUserQuery = 'Insert INTO Users (short, lastName, firstName, title, mailAddress, phoneNumber, birthDate, password, role) VALUES("'
+        + req.body.short +'","'
+        + req.body.lastName+'","'
+        + req.body.firstName+'","'
+        + req.body.title+'","'
+        + req.body.mailAddress+'","'
+        + req.body.phoneNumber+'","'
+        + req.body.birthDate+'","'
+        + req.body.password+'","'
+        + req.body.role+'")'
+        console.log(addUserQuery)
+        connection.query(addUserQuery, function (error, results, fields) {
+          if (error) throw error;
+          console.log(results)
+          res.send(results)
+        });
+      }
+    });
+  });
+});
+
+app.post('/addItem', async (req, res) => {
+  connection.getConnection(function (err, connection) {
+    const addItemQuery = 'Insert INTO Items (type, name, description, image, dateOfPurchase, storageSite) VALUES("'
+    + req.body.type +'","'
+    + req.body.name+'","'
+    + req.body.description+'","'
+    + req.body.image+'","'
+    + req.body.dateOfPurchase+'","'
+    + req.body.storageSite+'")'
+    console.log(addItemQuery)
+    let result
+    connection.query(addItemQuery, function (error, results, fields) {
+      if (error) throw error;
+      console.log(results)
+      if(results && req.body.damages && req.body.damages != null){
+        let itemID = results.insertId
+        let date = dayjs().format('YYYY-MM-DD')
+        let addDamagesQuery = 'INSERT INTO Damages (item_ID, damageDescription, date) VALUES '
+        let splitDamages = req.body.damages.split('\n')
+        for(const damage of splitDamages){
+          addDamagesQuery = addDamagesQuery + '("' +itemID + '","' + damage + '","' + date + '"),'
+        }
+        addDamagesQuery = addDamagesQuery.substring(0,addDamagesQuery.length-1)
+        connection.query(addDamagesQuery, function (error, results, fields) {
+          if (error) throw error;
+          console.log(results)
+        });
+        }
+    });
+  });
+});
+
+
+app.get('/itemsList', async (req, res) => {
   // Connecting to the database.
   // Connecting to the database.
   connection.getConnection(function (err, connection) {
     // Executing the MySQL query (select all data from the 'users' table).
-    connection.query('SELECT * FROM Items', function (error, results, fields) {
+    connection.query('SELECT ID, type, name, user_short FROM Items', function (error, results, fields) {
       // If some error occurs, we throw an error.
       if (error) throw error;
-
       // Getting the 'response' from the database and sending it to our route. This is were the data is.
       res.send(results)
     });
   });
+});
+
+app.post('/itemsForUser', async (req, res) => {
+  connection.getConnection(function (err, connection) {
+    const itemForUserQuery = 'SELECT * FROM Items WHERE user_short = "' + req.body.short + '"'
+    connection.query(itemForUserQuery, function (error, results, fields) {
+      if (error) throw error;
+      console.log(results)
+      res.send(results)
+    });
+  });
+});
+
+app.post('/reservedItemsForUser', async (req, res) => {
+  connection.getConnection(function (err, connection) {
+    const itemForUserQuery = 'SELECT i.ID, i.name, i.type, r.ID AS reservationID FROM Items i INNER JOIN Reservations r ON i.ID = r.item_ID WHERE r.user_short = "' + req.body.short +  '" AND r.dateFrom >="' + dayjs().format('YYYY-MM-DD') + '"'
+    connection.query(itemForUserQuery, function (error, results, fields) {
+      if (error) throw error;
+      console.log(results)
+      res.send(results)
+    });
+  });
+});
+
+app.post('/itemById', async (req, res) => {
+  // Connecting to the database.
+  // Connecting to the database.
+  connection.getConnection(function (err, connection) {
+    // Executing the MySQL query (select all data from the 'users' table).
+    const itemsQuery = 'SELECT i.type, i.name, i.description, i.image, i.dateOfPurchase, i.storageSite, i.qrCode, u.title, u.firstName, u.lastName, (SELECT GROUP_CONCAT(damageDescription SEPARATOR "\n") FROM damages WHERE item_ID = "'+ req.body.id+'") AS damages FROM Items i LEFT JOIN users u on i.user_short = u.short WHERE i.id = "' + req.body.id + '"'
+    console.log(itemsQuery)
+    connection.query(itemsQuery, function (error, results, fields) {
+      if (error) throw error;
+      // If some error occurs, we throw an error.
+      console.log(results)
+      // Getting the 'response' from the database and sending it to our route. This is were the data is.
+      res.send(results)
+    });
+  });
+});
+
+app.get('/damagesList', async (req, res) => {
+  try {
+    // Execute a SQL query to join the Items and Damages tables
+    const query = `
+      SELECT 
+        Damages.damageDescription, 
+        Damages.ID AS damageID,
+        Damages.date AS damageDate, 
+        Items.name AS itemName, 
+        Items.description AS itemDescription 
+      FROM Damages
+      INNER JOIN Items ON Damages.item_ID = Items.ID
+      ORDER BY Damages.date DESC
+    `;
+
+    // Run the query
+    connection.query(query, function (error, results, fields) {
+      if (error) {
+        throw error;
+      }
+
+      // Send the results as JSON response
+      res.json(results);
+    });
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
   
 //   try {
